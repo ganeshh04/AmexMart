3a73923747a64267d3a6af1d795d252c4fc14aa1
Ganesh Gatmane <ganeshgatmane04@gmail.com>
2025-11-05 21:37:42 +0530

    add project

---
+++ src/main/java/com/amexmart/AmexMartApplication.java
@@ -0,0 +1,13 @@
+package com.amexmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmexMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmexMartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amexmart/AmexMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amexmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmexMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
